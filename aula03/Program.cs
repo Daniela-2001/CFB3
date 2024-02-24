74b2ba49736da6f0a84a320fe7cfa8ed1abbac2c
Daniela Duarte <danielatavaresd@gmail.com>
2024-02-24 12:40:13 +0000

    CFB3

---
+++ aula03/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace aula03
+{
+    internal class Program
+    {
+        static void Main(){
+            int num1, num2, res;
+
+            num1 = 10;
+            num2 = 2;
+            res = num1 * num2;
+
+            Console.WriteLine("A multiplicação de " +  num1 + " com " + num2 + " é igual a " + res);
+        }
+    }
+}
